abbb679646d7acb552052842970a484fa4d4b937
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-12-11 17:59:22 +0100

    database creation

---
+++ db_setup.sql
@@ -0,0 +1,10 @@
+-- creates a db for Salvator
+-- creates a user and password
+-- grants permission on salvator_db
+
+SET GLOBAL validate_password.policy=LOW;
+CREATE DATABASE IF NOT EXISTS salvator_db;
+CREATE USER IF NOT EXISTS 'salvator'@'localhost' IDENTIFIED BY 'salvator_v1.0';
+GRANT ALL PRIVILEGES ON salvator_db.* TO 'salvator'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'salvator'@'localhost';
+FLUSH PRIVILEGES;
