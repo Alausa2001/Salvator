6245c099d2b742f87ad30d87b75472eea219bda7
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-12-12 16:03:14 +0100

    table for the user's biodata

---
+++ models/user_med_info.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from models.basemodel import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import String, Integer, Column, ForeignKey, Float
+
+
+class UserMedInfo:
+    """contains the biodata of a user
+    first_name, Last_name, age, weight, height, genotype, blood-group
+    """
+    __tablename__ = 'biodata'
+    first_name = Column(String(75), nullable=False)
+    last_name = Column(String(75), nullable=False)
+    genotype = Column(String(4), nullable=False)
+    blood_group = Column(String(2), nullable=False)
+    age = Column(Integer, nulable=False)
+    height = Column(Float, nullable=True)
+    weight = Cloumn(Float, nullable=True)
+
