606fc5b4546843662c0c0b7798302bc067bf75c2
Bibest <bibhestee@gmail.com>
2022-12-26 16:06:44 +0100

    User registration: 80%

--- web_app/templates/forms.py
+++ web_app/templates/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import Length, DataRequired, Email, EqualTo
 
 
 class SignUpForm(FlaskForm):
-    """
-    SignUpForm : A flaskform instance to generate form using flask
-    """
-    username = StringField(label='username')
-    password1 = PasswordField(label='password')
-    password2 = PasswordField(label='password')
-    submit = SubmitField(label='submit')
+    """ SignUpForm : A flaskform instance to generate form using flask """
+    username = StringField(label='Username', validators=[DataRequired(), Length(min=2, max=30)])
+    email = EmailField(label="Email address", validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField(label='Confirm password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(label='SIGN UP')
