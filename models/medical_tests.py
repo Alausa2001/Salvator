fcf605f6c95b2c4ec5ad49de2d5cd87ffa50888a
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-01-04 22:53:11 +0100

    added infomation about the medical tests of a user

---
+++ models/medical_tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from models.basemodel import BaseModel, Base
+import sqlalchemy
+from sqlalchemy.orm import Column, String, foreignKey
+
+class MedicalTest(BaseModel, Base):
+    """contains information about the medical tests of a user"""
+    __tablename__ = "MedicalTest"
+    test_id = Column(String(256), ForeignKey('login.id'), nullable=False)
+    test_type = Column(String(256), nullable=False)
+    date = Column(String(256), nullable=False)
+    result = Column(String(1024), nullable=False)
+    other = Column(String(1024), nullable=True)
