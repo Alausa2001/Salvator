4425271a67071981baa2a46ba7985be0f4310bac
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2023-01-12 01:47:54 +0100

    Delete backup.sh

--- backup/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# database backup
-
-sudo mysqldump -uroot salvator_db -p > db_backup.sql
-tar -cfz $(date +"%d-%m-%Y".tar.gz) db_backup.sql
-git add $(date +"%d-%m-%Y".tar.gz)
-git commit -m "database backup"
-git push
